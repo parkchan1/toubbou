a7adf996795b1115b436249ead57c0c2213fb3ed
parkchan1 <103724145+parkchan1@users.noreply.github.com>
2023-05-19 18:20:45 +0900

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,88 @@
+// $(document).ready(function(){ 
+
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 500) {
+//       $('#header_wrap').addClass('header-scrolled');
+//     $('nav').fadeOut(0);
+//     $('.none').fadeIn(0);
+//     } 
+//     else {
+//       $('#header_wrap').removeClass('header-scrolled');
+//     $('nav').fadeIn(0);
+//     $('.none').fadeOut(0);
+//     }
+//   })
+ 
+//  });
+
+
+function modalOpen()
+{
+    $('#search_modal').show();
+};
+function modalClose()
+{
+    $('#search_modal').hide();
+};
+
+
+ $(function(){
+	$('.pc_nav> li, .header_tool >li ').mouseover(function(){
+		$(this).children('.pc_nav_sub').stop().slideDown();
+	});
+	$('.pc_nav> li, .header_tool >li').mouseleave(function(){
+		$(this).children('.pc_nav_sub').stop().slideUp();
+	});
+});
+
+
+
+
+$(function(){
+  $("#button1").click(function(){
+          $(".place_now1").fadeIn();
+          $(".place_now2").fadeOut();
+          $(".place_now3").fadeOut();
+  });	
+  $("#button2").click(function(){
+          $(".place_now1").fadeOut();
+          $(".place_now2").fadeIn();
+          $(".place_now3").fadeOut();
+  });
+  $("#button3").click(function(){
+          $(".place_now1").fadeOut();
+          $(".place_now2").fadeOut();
+          $(".place_now3").fadeIn();
+  });
+
+});
+
+
+// m-nav
+$(document).ready(function(){
+ 
+  $('.m-menu_btn').on('click', function(){
+      $('.m-menu_bg').show();
+      $('.sidebar_menu').show().animate({
+          left:0
+      });  
+      $('html, body').addClass('hidden');
+  });
+
+  $('.close_btn').on('click', function(){
+      $('.m-menu_bg').hide(); 
+      $('.sidebar_menu').animate({
+          left: '-' + 80 + '%'
+                  },function(){
+                      $('.sidebar_menu').hide();          
+                  });
+      $('html, body').removeClass('hidden')  
+  });
+  document.querySelector('.m-menu_bg').addEventListener('click', function(e){
+      document.querySelector(".close_btn").click();
+  });             
+
+
+});
+
+
